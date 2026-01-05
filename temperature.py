aa435341bf7bc88637712e3d82d47993c6d3a620
suhejlkoltraka <suhejl.koltraka@cit.edu.al>
2026-01-05 14:35:53 +0100

    temperature.py

---
+++ temperature.py
@@ -0,0 +1,11 @@
+# Temperature conversion
+
+celsius = 25
+fahrenheit = (celsius * 9/5) + 32
+
+print(celsius, "°C =", fahrenheit, "°F")
+
+fahrenheit2 = 77
+celsius2 = (fahrenheit2 - 32) * 5/9
+
+print(fahrenheit2, "°F =", celsius2, "°C")
